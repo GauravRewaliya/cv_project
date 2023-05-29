9cb6a50518774d9153c7ed3b7af917318b6e5dcb
GauravR <GauravRewaliya@gmail.com>
2023-05-29 12:59:22 +0530

    "next just need UI for CV"

---
+++ app/controllers/curriculum_vitaes_controller.rb
@@ -0,0 +1,87 @@
+class CurriculumVitaesController < ApplicationController
+  before_action :set_curriculum_vitae, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+
+  def index
+    @curriculum_vitaes = CurriculumVitae.all
+  end
+
+  
+  def show
+  end
+
+  
+  def new
+    @curriculum_vitae = CurriculumVitae.new
+  end
+
+  
+  def edit
+  end
+
+  
+  def create
+
+    @curriculum_vitae = CurriculumVitae.new(curriculum_vitae_params)
+
+    c = nil
+    if !params[:candidate_id].blank?
+    c = Candidate.find(params[:candidate_id])
+    end
+    @curriculum_vitae.candidate = c 
+
+    respond_to do |format|
+      if @curriculum_vitae.save
+        format.html { redirect_to curriculum_vitae_url(@curriculum_vitae), notice: "CurriculumVitae was successfully created." }
+        format.json { render :show, status: :created, location: @curriculum_vitae }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @curriculum_vitae.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  
+  def update
+
+    c = nil
+    if !params[:candidate_id].blank?
+    c = Candidate.find(params[:candidate_id])
+    end
+    @curriculum_vitae.candidate = c 
+    
+    respond_to do |format|
+      if @curriculum_vitae.update(curriculum_vitae_params)
+        format.html { redirect_to curriculum_vitae_url(@curriculum_vitae), notice: "CurriculumVitae was successfully updated." }
+        format.json { render :show, status: :ok, location: @curriculum_vitae }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @curriculum_vitae.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  
+  def destroy
+    # cant destroy directly 
+    @curriculum_vitae.candidate =nil 
+    @curriculum_vitae.project_ids = [] 
+
+    @curriculum_vitae.destroy
+    respond_to do |format|
+      format.html { redirect_to curriculum_vitaes_url, notice: "CurriculumVitae was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  
+    def set_curriculum_vitae
+      @curriculum_vitae = CurriculumVitae.find(params[:id])
+    end
+
+    
+    def curriculum_vitae_params
+      params.require(:curriculum_vitae).permit(project_ids: [])                                                                                #  curriculum_vitae_core_tech me tech_stack_id lega
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # resources :skills ,:candidates ,:profiles
   devise_for :users
   resources :tech_stacks ,:candidates ,:profiles
+  resources :curriculum_vitaes
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

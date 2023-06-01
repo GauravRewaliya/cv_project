90e9b404bd6922f934761a1bfd2c76619624c63b
GauravR <GauravRewaliya@gmail.com>
2023-06-01 15:01:36 +0530

    "candidate dependent cv destroy"

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,9 +1,22 @@
 class Candidate < ApplicationRecord
     has_one :profile , dependent: :destroy
     accepts_nested_attributes_for :profile
-
+    #cand hasmany cv so i delete cv releted to it
+    has_many :candidate_curriculum_vitaes 
+    has_many :curriculum_vitaes , through: :candidate_curriculum_vitaes 
     before_destroy :delete_fun
     def delete_fun
-        CandidateCurriculumVitae.where( candidate_id: self.id).destroy_all
+        #destroy all cvs related to candidate
+        a_ids = self.curriculum_vitae_ids
+        CurriculumVitae.destroy(a_ids)
     end
+    # def delete_fun
+        # self.curriculum_vitaes.destroy_all
+        # candidate_curriculum_vitaes.each do |ccv|
+        #     ccv.curriculum_vitae.destroy
+        #     ccv.destroy
+        #   end
+        #   curriculum_vitaes.each(&:destroy)
+        # CandidateCurriculumVitae.where( candidate_id: self.id).destroy_all
+    # end
 end

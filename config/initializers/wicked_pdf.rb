f2b47d98cab20eb5292a93fcfcaae90bb79e9a2d
GauravR <GauravRewaliya@gmail.com>
2023-07-19 18:10:02 +0530

    "+ basicCvTemplate,fix emptHidn fild, groupselect"

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,7 +1,7 @@
 WickedPdf.config = {
     exe_path: '/home/gr/.rvm/gems/ruby-3.2.0/bin/wkhtmltopdf',
-    margin: { top:    0,      bottom: 2,
-              left:   0,      right:  0 },
+    margin: { top:    4,      bottom: 2,
+              left:   1,      right:  1 },
       page_size: 'A4'
       # orientation: 'Landscape'
   }

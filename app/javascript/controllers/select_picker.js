05b7a560f487e8eba0744d85598d36b12edffe1d
GauravR <GauravRewaliya@gmail.com>
2023-07-28 14:46:28 +0530

    "got responsive bug(background img , 1 file)"

--- app/javascript/controllers/select_picker.js
+++ app/javascript/controllers/select_picker.js
@@ -1,6 +1,8 @@
 function select_picker_refresh()
 {
-    setTimeout(() =>{ $('.selectpick').selectpicker({ liveSearch: true});
+    setTimeout(() =>{ 
+                $('.selectpick').selectpicker({ liveSearch: true});
+                $('.selectpick').selectpicker('render');
                     }, "100");
 }
 // load by cvJs_loader

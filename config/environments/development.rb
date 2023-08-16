268e9aa72f7b8ff63cb7305979de4247cb156cad
GauravR <gauravRewaliya@gmail.com>
2023-08-16 18:50:27 +0530

    compressed js css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,8 +5,8 @@ Rails.application.configure do
   config.serve_static_assets = false
 
   config.assets.debug = true  # got updated assets every time
-  config.serve_static_files = false # For Rails 4.x
-  config.public_file_server.enabled = true # For Rails 5.x and later
+  # config.serve_static_files = false # For Rails 4.x
+  # config.public_file_server.enabled = true # For Rails 5.x and later
 
   # Settings specified here will take precedence over those in config/application.rb.
 
